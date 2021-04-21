9397af36ac49cfb3722f15ed495852d69550761f
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-04-21 22:37:46 +1000

    Catch error

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const data = JSON.parse(process.argv[2]);
-console.log(data);
+try {
+  const data = JSON.parse(process.argv[2]);
+  console.log(data);
+} catch {
+  console.log('Unable to parse');
+}
